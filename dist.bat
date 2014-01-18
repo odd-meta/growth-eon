6a6f12c548fd6ce3e90960246085baa350dffe74
odd-meta <odd.meta@gmail.com>
2014-01-17 23:10:52 -0500

    added ignore for love folder

--- dist.bat
+++ dist.bat
@@ -6,11 +6,17 @@ mkdir dist\game
 
 xcopy game\* dist\game\ /E
 
-xcopy love\*.dll dist\game\
-
 cd dist\game
 "%programfiles%\7-Zip\7z" a -tzip ..\game.love *
 cd ..\..
 
+mkdir dist\growth-eon
+copy /b love\love.exe+dist\game.love dist\growth-eon\growth-eon.exe
 
-copy /b love\love.exe+dist\game.love dist\growth-eon.exe
+
+xcopy love\*.dll dist\growth-eon\
+
+cd dist
+
+"%programfiles%\7-Zip\7z" a -tzip growth-eon.zip growth-eon\*
+pause
